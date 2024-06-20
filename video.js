5ed89d4221c9e8dba32623145c8d55fedb2231f3
yogurtsmootie <some9915@gmail.com>
2024-06-21 00:12:06 +0900

    Add files via upload

---
+++ video.js
@@ -0,0 +1,27 @@
+function playVideo(videoSrc) {
+    var videoPlayer = document.getElementById('videoPlayer');
+    var videoElement = document.getElementById('videoElement');
+    var videoSource = document.getElementById('videoSource');
+    
+    videoSource.src = videoSrc;
+    videoElement.load();
+    videoElement.play();  // 비디오 자동 재생
+    videoPlayer.style.display = 'block';
+}
+
+function closeVideo() {
+    var videoPlayer = document.getElementById('videoPlayer');
+    var videoElement = document.getElementById('videoElement');
+    
+    videoElement.pause();
+    videoPlayer.style.display = 'none';
+}
+
+function closeVideoByClickOutside(event) {
+    var videoPlayer = document.getElementById('videoPlayer');
+    var videoElement = document.getElementById('videoElement');
+
+    if (!videoElement.contains(event.target)) {
+        closeVideo();
+    }
+}
